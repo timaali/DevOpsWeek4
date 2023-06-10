4d47a8bb03a0bb4a7a71b09c934496f71579c7b5
TimaAli <timaali43@gmail.com>
2023-06-10 12:08:21 +0300

    This commit adds variable in the terraform variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable "docker_images" {
     type        = list(string)
     description = "This are the images in my enviroment"
     default = [
-        "",
-        "",
+        "timaali/client:latest",
+        "timaali/backend:latest",
     ]
 }
